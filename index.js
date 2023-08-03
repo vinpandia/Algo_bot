5ec89d7beea76278f4543d0965a0e9dc5b4033dc
Vinayak pandia <vinayakpandia@Vinayaks-MacBook-Air-2.local>
2023-08-03 17:51:39 +0530

    added logit for all searching and sorting

--- index.js
+++ index.js
@@ -6,8 +6,28 @@ const bot = new Telegraf('6669277595:AAGQlhqt6_oGQwc6n9ejJfuPRYv6ajDCikk');
 
 bot.start((ctx) => ctx.reply('Welcome to Algo_bot made by Vinayak \n This bot will send you most optimized code for searching and sorrting algorithms'));
 
-bot.command('BinarySearchjs' , async function(ctx){
+bot.command('BinarySearch' , async function(ctx){
     const response = await axios.get('https://raw.githubusercontent.com/vinpandia/algorithms/main/Binary%20Search');
+    console.log(response.data);
+    return ctx.reply(response.data);
+})
+bot.command('LinearSearch' , async function(ctx){
+    const response = await axios.get('https://raw.githubusercontent.com/vinpandia/algorithms/main/linear%20serach');
+    console.log(response.data);
+    return ctx.reply(response.data);
+})
+bot.command('QuickSort' , async function(ctx){
+    const response = await axios.get('https://raw.githubusercontent.com/vinpandia/algorithms/main/quick%20sort');
+    console.log(response.data);
+    return ctx.reply(response.data);
+})
+bot.command('SelectionSort' , async function(ctx){
+    const response = await axios.get('https://raw.githubusercontent.com/vinpandia/algorithms/main/selection%20sort');
+    console.log(response.data);
+    return ctx.reply(response.data);
+})
+bot.command('InsertionSort' , async function(ctx){
+    const response = await axios.get('https://raw.githubusercontent.com/vinpandia/algorithms/main/insertion%20sort');
     console.log(response.data);
     return ctx.reply(response.data);
 })
